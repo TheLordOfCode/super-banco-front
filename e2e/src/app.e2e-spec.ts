c8ea3f5c87d9f607b23dc2622a1c725a945ad7e2
Juan Guillermo Quevedo Sánchez - Ceiba Software <juan.quevedo@ceiba.com.co>
2021-03-30 11:05:08 -0500

    version 1: super-banco-front

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('sistema-vacunacion-covid app is running!');
+    expect(await page.getTitleText()).toEqual('sistema-calculo-incremento-capital app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sistema-vacunacion-covid'),
+      dir: require('path').join(__dirname, './coverage/sistema-calculo-incremento-capital'),
       subdir: '.',
       reporters: [
         { type: 'html' },
